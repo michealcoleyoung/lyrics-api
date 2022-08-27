b12d9330c7903dc46c73e00bd7fad347f3441537
michealcoleyoung <coleyoungmusic@gmail.com>
2022-08-27 13:25:21 -0500

    Installed genius-lyrics-api npm package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
+import axios from 'axios';
+import { getLyrics, getSong } from 'genius-lyrics-api';
 import './App.css';
 
 function App() {
+
+  const options = {
+    apiKey: '154904640fmsh615882c14b253d1p1b934cjsnab983c8da167',
+    title: 'Blinding Lights',
+    artist: 'The Weeknd',
+    optimizeQuery: true
+  };
+  
+  getLyrics(options).then((lyrics) => console.log(lyrics));
+  
+  getSong(options).then((song) =>
+    console.log(`
+    ${song.id}
+    ${song.title}
+    ${song.url}
+    ${song.albumArt}
+    ${song.lyrics}`)
+  );
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text"  placeholder={'Artist'}/><br></br><br />
+      <input type="text"  placeholder={'Song'}/>
+      <h1></h1>
     </div>
   );
 }
