1bc94219ae284371a9582cf1fd1d58853c59210d
michealcoleyoung <coleyoungmusic@gmail.com>
2023-10-15 15:19:36 -0500

    Added conditionals for search

--- app.py
+++ app.py
@@ -7,30 +7,53 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
+    api = AZlyrics("google")
+    # if request.method == 'POST':
+
+    #     # user song search
+    #     api.title = request.form['search_lyrics']
+    #     # api.artist = request.form['search_artist']
+
+    #     # correct formatting of printed lyrics
+
+    #     if api.title:
+    #         lyrics = api.getLyrics()
+    #         formatted_lyrics = lyrics.replace('\n', '<br>')
+    #         return render_template('index.html',song=api.title, lyrics=formatted_lyrics)
+    #     elif api.title == '':
+    #         error_message = "Please enter the name of the song you want lyrics for"
+    #         return render_template('index.html', error_message=error_message)
+    #     else:
+    #         return "<h1>There are no songs by this name</h1>"
     if request.method == 'POST':
-        api = AZlyrics("google")
 
         # user song search
+        api.artist = request.form['search_artist']
         api.title = request.form['search_lyrics']
-        # api.artist = request.form['search_artist']
-
-        
-
+    
         # correct formatting of printed lyrics
 
-        if api.title:
+
+        if api.artist == '' and api.title == '':
+            print("========= ARTIST FIELD AND SONG FIELD ARE BLANK =========")
+            error_message = "Please enter either an artist or song name"
+            return render_template('index.html', error_message=error_message)
+        elif api.artist and api.title == '':
+            print("========= ARTIST FIELD IS NOT BLANK AND SONG FIELD IS BLANK  =========")
+            songs = api.getSongs()
+            print(songs)
+            return render_template('index.html',songs=songs)
+        elif api.artist == '' and api.title:
+            print("========= ARTIST FIELD IS BLANK AND SONG FIELD IS NOT BLANK  =========")
             lyrics = api.getLyrics()
             formatted_lyrics = lyrics.replace('\n', '<br>')
             return render_template('index.html',song=api.title, lyrics=formatted_lyrics)
-        elif api.title == '':
-            return render_template('index.html')
         else:
-            return "<h1>There are no songs by this name</h1>"
-        
+            print("========= ARTIST FIELD SONG FIELDS ARE NOT BLANK  =========")
+            lyrics = api.getLyrics(save=True)
+            formatted_lyrics = lyrics.replace('\n', '<br>')
+            return render_template('index.html',song=api.title, lyrics=formatted_lyrics)     
 
-    return render_template('index.html')
-
-    
 
 if __name__ == "__main__":
     app.run(debug=True)
