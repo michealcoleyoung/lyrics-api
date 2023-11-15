3b7d0cc9676e23d999cfa14dc50c6aecc82cc0cd
michealcoleyoung <coleyoungmusic@gmail.com>
2023-11-14 19:34:22 -0600

    Set additional artist and lyric field checks along with corresponding error messages

--- app.py
+++ app.py
@@ -23,19 +23,28 @@ def main():
         elif api.artist and api.title == '':
             songs = api.getSongs()
             print(songs)
-            return render_template('index.html',songs=songs)
+            error_message = "We cannot find an artist by that name"
+            return render_template('index.html',songs=songs, error_message=error_message)
         
         # ARTIST FIELD IS BLANK AND SONG FIELD IS NOT BLANK
         elif api.artist == '' and api.title:
             lyrics = api.getLyrics()
-            formatted_lyrics = lyrics.replace('\n', '<br>')
-            return render_template('index.html',song=api.title, lyrics=formatted_lyrics)
+            if isinstance(lyrics, str):
+                formatted_lyrics = lyrics.replace('\n', '<br>')
+                return render_template('index.html',song=api.title, lyrics=formatted_lyrics)
+            else:
+                error_message = "An error occurred while retrieving lyrics. Please try again."
+                return render_template('index.html', error_message=error_message)
         
         # ARTIST FIELD SONG FIELDS ARE NOT BLANK
         else:
             lyrics = api.getLyrics()
-            formatted_lyrics = lyrics.replace('\n', '<br>')
-            return render_template('index.html',song=api.title, lyrics=formatted_lyrics)
+            if isinstance(lyrics, str):
+                formatted_lyrics = lyrics.replace('\n', '<br>')
+                return render_template('index.html',song=api.title, lyrics=formatted_lyrics)
+            else:
+                error_message = "An error occurred while retrieving lyrics. Please try again."
+                return render_template('index.html', error_message=error_message)
     else:
         return render_template("index.html")
     
@@ -54,8 +63,5 @@ def get_lyrics():
 
     return render_template('index.html', song=title, lyrics=formatted_lyrics)
 
-     
-
-
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
